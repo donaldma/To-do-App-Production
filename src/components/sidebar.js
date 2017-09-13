5acf6d1900a8fdab516cfaab27d3d716db949d3f
donaldma <donald.ma@hotmail.ca>
2017-09-13 07:05:56 -0700

    removed

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,7 +6,7 @@ export default class SideBar extends Component {
   render() {
     return (
       <div className="panel panel-default">
-        <div className="panel-heading"><input type="text" className="form-control" placeholder="Search" /></div>
+        <div className="panel-heading">Users</div>
         <ul className="list-group">
           {this.props.renderUsers()}
         </ul>
