aeb4359784b6b510aaeca9e32af77a38decfb200
donaldma <donald.ma@hotmail.ca>
2017-09-14 09:31:34 -0700

    improved mobile responsiveness for navbar links

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,15 +3,9 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
 import * as actions from '../actions';
+import SideNav from 'react-simple-sidenav';
 
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showNav: false
-    }
-  }
-
   renderUsers = () => {
     if(_.size(this.props.users) === 0) {
       return(
@@ -31,6 +25,35 @@ class NavBar extends Component {
   }
 
   render() {
+    if(_.size(this.props.users) === 0) {
+      return (
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <ul className="navbar-toggle nav-logo">
+                <li><a href="/">To Do</a></li>
+              </ul> 
+              <button type="button" className="navbar-toggle nav-menu" data-toggle="collapse" data-target="#myNavbar">
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>                        
+              </button>
+            </div>
+            <div className="collapse navbar-collapse" id="myNavbar">
+              <ul className="nav navbar-nav">
+                <li className="full-nav"><a href="/">To Do</a></li>
+              </ul>
+              <ul className="nav navbar-nav navbar-right">
+                <li className="full-nav"><Link to={'/'}><i className="fa fa-user-circle-o" aria-hidden="true"></i> Users Tasks</Link></li>
+                <li className="full-nav"><Link to={'/tasks'}><i className="fa fa-list" aria-hidden="true"></i> All Tasks</Link></li>
+                <li className="mobile-nav"><a href='/'><i className="fa fa-user-circle-o" aria-hidden="true"></i> Users Tasks</a></li>
+                <li className="mobile-nav"><a href='/tasks'><i className="fa fa-list" aria-hidden="true"></i> All Tasks</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      );
+    }
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
         <div className="container">
@@ -49,10 +72,12 @@ class NavBar extends Component {
               <li className="full-nav"><a href="/">To Do</a></li>
             </ul>
             <ul className="nav navbar-nav navbar-right">
-              <li><Link to={'/'}><i className="fa fa-user-circle-o" aria-hidden="true"></i> Users Tasks</Link></li>
-              <li><Link to={'/tasks'}><i className="fa fa-list" aria-hidden="true"></i> All Tasks</Link></li>
+              <li className="full-nav"><Link to={'/'}><i className="fa fa-user-circle-o" aria-hidden="true"></i> Users Tasks</Link></li>
+              <li className="full-nav"><Link to={'/tasks'}><i className="fa fa-list" aria-hidden="true"></i> All Tasks</Link></li>
+              <li className="mobile-nav"><a href='/'><i className="fa fa-user-circle-o" aria-hidden="true"></i> Users Tasks</a></li>
+              <li className="mobile-nav"><a href='/tasks'><i className="fa fa-list" aria-hidden="true"></i> All Tasks</a></li>
               <li className="seperator"></li>
-              {this.renderUsers()}
+              {this.renderUsers()}              
             </ul>
           </div>
         </div>
